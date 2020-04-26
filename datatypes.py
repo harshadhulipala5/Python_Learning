5b5954df798438f3a3b6f4a96bb5b0466ece096e
Harsha <harshadhulipala5@gmail.com>
2020-04-26 19:28:29 +0530

    data types and uses

---
+++ datatypes.py
@@ -0,0 +1,28 @@
+#!/bin/python
+# Data types are of two types
+#     Implicit
+#         primitive type
+#             Numbers
+#                 int
+#                 float
+#                 complex
+#                 long
+#             Non-Number
+#                 str
+#                 unicode
+#                 bool
+#                 byte
+#         Data Structure type
+#             list
+#             tuple
+#             dictionary
+#     Explicit
+a=10
+b=20.10
+c=10+20j
+d=1012312432425433
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
